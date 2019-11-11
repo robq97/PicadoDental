3630391f13037caceaaf40da6632b22de7a23c70
Roberto <robq97@hotmail.com>
2019-11-10 23:01:12 -0600

    Se agregaron todos los modelos y sus propiedades

---
+++ PicadoDental/PicadoDental/Models/MCliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PicadoDental.Models
+{
+    public class Mcliente
+    {
+        public short Clienteid { get; set; }
+        public short Personaid { get; set; }
+    }
+}
---
+++ PicadoDental/PicadoDental/Models/MEspecialidad.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PicadoDental.Models
+{
+    public class MEspecialidad
+    {
+        public short Especialistaid { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
