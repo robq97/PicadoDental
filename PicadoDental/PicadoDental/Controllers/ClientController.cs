48ed9d548f6c578e8227299f6a68813d36071d22
Brad <bradguillen15@gmail.com>
2019-11-14 23:53:03 -0600

    connect newClient to ws and add Doctor views based on Client

--- PicadoDental/PicadoDental/Controllers/ClientController.cs
+++ PicadoDental/PicadoDental/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,14 +9,31 @@ namespace PicadoDental.Controllers
 {
     public class ClientController : Controller
     {
+        EF_PicadoDental_WS.EF_PicadoDentalSoapClient WS = new EF_PicadoDental_WS.EF_PicadoDentalSoapClient();
+
         // GET: Cliente
         public ActionResult NewClient()
         {
             return View();
         }
 
+        public ActionResult AddNewClient(
+            string FirstName,
+            string LastName,
+            string Id,
+            string Phone,
+            string Email,
+            string Gender)
+        {
+            WS.NewClient(FirstName, LastName, "", Phone, Email, int.Parse(Gender));
+
+            return RedirectToAction("ClientList", "Client");
+        }
+
         public ActionResult ClientList()
         {
+            //var clientList = WS.ClientList();
+
             return View();
         }
 
---
+++ PicadoDental/PicadoDental/Controllers/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PicadoDental.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Cliente
+        public ActionResult NewDoctor()
+        {
+            return View();
+        }
+
+        public ActionResult DoctorList()
+        {
+            return View();
+        }
+    }
+}
